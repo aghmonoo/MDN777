45bd6042eb9843d11369ac641ecd3392283c65c3
Aung Mon Oo <aghmonoo@gmail.com>
2026-05-19 10:12:58 +0700

    Deploy admin polish

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "662454712" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1439254119" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
